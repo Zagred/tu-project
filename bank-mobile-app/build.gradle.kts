3d7cf66cf2a5b3730d0b2538de54bfdf054804ea
Plamen <ppandev5@gmail.com>
2025-10-08 13:04:05 +0300

    .

--- bank-mobile-app/build.gradle.kts
+++ bank-mobile-app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-
     id("org.sonarqube") version "4.4.1.3373"
 }
+
 sonarqube {
     properties {
         property("sonar.projectKey", "bank-mobile-app")
@@ -13,16 +10,10 @@ sonarqube {
         property("sonar.login", System.getenv("SONAR_TOKEN"))
         property("sonar.sourceEncoding", "UTF-8")
 
-        // Correctly pass sources as a list
-        property("sonar.sources", listOf("app/src/main/java"))
-        property("sonar.tests", listOf("app/src/test/java"))
+        property("sonar.sources", "app/src/main/java,app/src/main/kotlin")
+        property("sonar.tests", "app/src/test/java,app/src/test/kotlin")
 
-        // Skip Kotlin build scripts
         property("sonar.kotlin.ignoreBuildScripts", true)
-
-        // Optional: skip compile step if needed
         property("sonar.gradle.skipCompile", true)
     }
 }
-
-
