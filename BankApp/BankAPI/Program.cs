7622ba9a51ab955e69600574cb54652b1ed5e82d
paco <looopooo45@gmail.com>
2026-05-18 11:32:07 +0300

    Enable Swagger for deployed API

--- BankApp/BankAPI/Program.cs
+++ BankApp/BankAPI/Program.cs
@@ -96,10 +96,11 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseHttpsRedirection();
 }
 
